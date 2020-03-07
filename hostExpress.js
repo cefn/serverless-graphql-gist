8b45fed783051ef9835ab5b3e22a5bcfef99d0c1
Cefn Hoile <github.com@cefn.com>
2020-03-07 00:07:22 +0000

    Change default express port

--- hostExpress.js
+++ hostExpress.js
@@ -2,7 +2,7 @@ const {createServer} = require("./graphqlExpressServer")
 
 const expressServer = createServer()
 
-const port = 3000
+const port = 3001
 
 expressServer.listen(port, err => {
   if (err) throw err
